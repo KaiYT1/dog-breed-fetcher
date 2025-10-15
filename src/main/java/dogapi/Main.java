047daf8a00fe0257beb5dfd00550a503ca9c37e1
KaiYT1 <kaibague@gmail.com>
2025-10-14 22:13:52 -0400

    small changes to test our code

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         String breed = "hound";
-        BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
+        BreedFetcher breedFetcher = new DogApiBreedFetcher();
+        BreedFetcher breedcachingFetcher = new CachingBreedFetcher(breedFetcher);
         int result = getNumberOfSubBreeds(breed, breedFetcher);
         System.out.println(breed + " has " + result + " sub breeds");
 
