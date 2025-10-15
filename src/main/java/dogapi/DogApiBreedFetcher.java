496a59df1265242b931f8c562301479f3f41bc52
KaiYT1 <kaibague@gmail.com>
2025-10-14 20:51:07 -0400

    fix imports

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -2,7 +2,6 @@ package dogapi;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
